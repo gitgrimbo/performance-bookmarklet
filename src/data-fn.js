6315c1d07dcfe1dc5372d9cfe328d03659848821
gitgrimbo <gitgrimbo@gmail.com>
2018-04-01 10:28:40 +0100

    Extract a data-processing function that requires no dom

---
+++ src/data-fn.js
@@ -0,0 +1,153 @@
+import helper from "./helpers/helpers";
+
+/**
+ * @param documentDomain {string}
+ * @param locationHost {string}
+ * @param locationHref {string}
+ */
+export default function(documentDomain, locationHost, locationHref, perfTiming, resources, marks, measures) {
+
+var data = {
+	resources,
+	marks,
+	measures,
+	perfTiming,
+	allResourcesCalc: []
+};
+
+var isValid = true;
+
+data.isValid = function(){
+	return isValid;
+}
+
+if(data.perfTiming.loadEventEnd - data.perfTiming.navigationStart < 0){
+	alert("Page is still loading - please try again when page is loaded.");
+	isValid = false;
+	return;
+}
+
+
+data.allResourcesCalc = data.resources
+	//remove this bookmarklet from the result
+	.filter((currR) => !currR.name.match(/http[s]?\:\/\/(micmro|nurun).github.io\/performance-bookmarklet\/.*/)) 
+	.map((currR, i, arr) => {
+		//crunch the resources data into something easier to work with
+		const isRequest = currR.name.indexOf("http") === 0;
+		var urlFragments, maybeFileName, fileExtension;
+
+		if(isRequest){
+			urlFragments = currR.name.match(/:\/\/(.[^/]+)([^?]*)\??(.*)/);
+			maybeFileName = urlFragments[2].split("/").pop();
+			fileExtension = maybeFileName.substr((Math.max(0, maybeFileName.lastIndexOf(".")) || Infinity) + 1);
+		}else{
+			urlFragments = ["", locationHost];
+			fileExtension = currR.name.split(":")[0];
+		}
+
+		var currRes = {
+			name : currR.name,
+			domain : urlFragments[1],
+			initiatorType : currR.initiatorType || fileExtension || "SourceMap or Not Defined",
+			fileExtension : fileExtension || "XHR or Not Defined",
+			loadtime : currR.duration,
+			fileType : helper.getFileType(fileExtension, currR.initiatorType),
+			isRequestToHost : urlFragments[1] === locationHost
+		};
+
+		for(let attr in currR){
+			if(typeof currR[attr] !== "function") {
+				currRes[attr] = currR[attr];
+			}
+		}
+
+		if(currR.requestStart){
+			currRes.requestStartDelay = currR.requestStart - currR.startTime;
+			currRes.dns = currR.domainLookupEnd - currR.domainLookupStart;
+			currRes.tcp = currR.connectEnd - currR.connectStart;
+			currRes.ttfb = currR.responseStart - currR.startTime;
+			currRes.requestDuration = currR.responseStart - currR.requestStart;
+		}
+		if(currR.secureConnectionStart){
+			currRes.ssl = currR.connectEnd - currR.secureConnectionStart;
+		}
+		
+		return currRes;
+	});
+
+//filter out non-http[s] and sourcemaps
+data.requestsOnly = data.allResourcesCalc.filter((currR) => {
+	return currR.name.indexOf("http") === 0 && !currR.name.match(/js.map$/);
+});
+
+
+//get counts
+data.initiatorTypeCounts = helper.getItemCount(data.requestsOnly.map((currR, i, arr) => {
+	return currR.initiatorType || currR.fileExtension;
+}), "initiatorType");
+
+data.initiatorTypeCountHostExt = helper.getItemCount(data.requestsOnly.map((currR, i, arr) => {
+	return (currR.initiatorType  || currR.fileExtension) + " " + (currR.isRequestToHost ? "(host)" : "(external)");
+}), "initiatorType");
+
+data.requestsByDomain = helper.getItemCount(data.requestsOnly.map((currR, i, arr) => currR.domain), "domain");
+
+data.fileTypeCountHostExt = helper.getItemCount(data.requestsOnly.map((currR, i, arr) => {
+	return currR.fileType  + " " + (currR.isRequestToHost ? "(host)" : "(external)");
+}), "fileType");
+
+data.fileTypeCounts = helper.getItemCount(data.requestsOnly.map((currR, i, arr) => currR.fileType), "fileType");
+
+var tempResponseEnd = {};
+//TODO: make immutable
+data.requestsOnly.forEach((currR) => {
+	var entry = data.requestsByDomain.filter((a) => a.domain == currR.domain)[0]||{};
+
+	var lastResponseEnd = tempResponseEnd[currR.domain]||0;
+
+	currR.duration = entry.duration||(currR.responseEnd - currR.startTime);
+
+	if(lastResponseEnd <= currR.startTime){
+		entry.durationTotalParallel = (entry.durationTotalParallel||0) + currR.duration;
+	} else if (lastResponseEnd < currR.responseEnd){
+		entry.durationTotalParallel = (entry.durationTotalParallel||0) + (currR.responseEnd - lastResponseEnd);
+	}
+	tempResponseEnd[currR.domain] = currR.responseEnd||0;
+	entry.durationTotal = (entry.durationTotal||0) + currR.duration;
+});
+
+
+//Request counts
+data.hostRequests = data.requestsOnly
+	.filter((domain) => domain.domain === locationHost).length;
+
+data.currAndSubdomainRequests = data.requestsOnly
+	.filter((domain) => domain.domain.split(".").slice(-2).join(".") === locationHost.split(".").slice(-2).join("."))
+	.length;
+
+data.crossDocDomainRequests = data.requestsOnly
+	.filter((domain) => !helper.endsWith(domain.domain, documentDomain)).length;
+
+data.hostSubdomains = data.requestsByDomain
+	.filter((domain) => helper.endsWith(domain.domain, locationHost.split(".").slice(-2).join(".")) && domain.domain !== locationHost)
+	.length;
+
+
+data.slowestCalls = [];
+data.average = undefined;
+
+if(data.allResourcesCalc.length > 0){
+	data.slowestCalls = data.allResourcesCalc
+		.filter((a) => a.name !== locationHref)
+		.sort((a, b) => b.duration - a.duration);
+
+	data.average = Math.floor(data.slowestCalls.reduceRight((a,b) => {
+		if(typeof a !== "number"){
+			return a.duration + b.duration
+		}
+		return a + b.duration;
+	}) / data.slowestCalls.length);
+}
+
+return data;
+};
